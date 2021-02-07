6ac2a0a304bdea3bbb5d97f5d4d13e01789f2a71
bosshentai <baptistamhernani@gmail.com>
2021-02-07 21:30:30 -0100

    have add file challenge1 and challenge2 it was made for calcule all the BIG O of each file

--- nemo.js
+++ nemo.js
@@ -1,17 +1,27 @@
-const { performance } = require('perf_hooks');
+const { performance } = require("perf_hooks");
 
-const nemo = ['nemo']
-const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt','darla','hank'];
+const nemo = ["nemo"];
+const everyone = [
+  "dory",
+  "bruce",
+  "marlin",
+  "nemo",
+  "gill",
+  "bloat",
+  "nigel",
+  "squirt",
+  "darla",
+  "hank",
+];
 
-const large = new Array(100000).fill('nemo');
+const large = new Array(100000).fill("nemo");
 
-function findNemo(array){
-   // let t0 = performance.now();
-    for (let i = 0 ; i < array.length; i++){
-        if (array[i] === 'nemo')
-        console.log('Found Nemo!');
-    }
-   // let t1 = performance.now()
+function findNemo(array) {
+  // let t0 = performance.now();
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "nemo") console.log("Found Nemo!");
+  }
+  // let t1 = performance.now()
 
   //  console.log('Call to find Nemo took ' + (t1 - t0) + ' milliseconds')
 }
@@ -20,11 +30,9 @@ function findNemo(array){
 
 //findNemo(everyone);
 
-findNemo(large);  // O(n) --> Linear Time
-
-
+findNemo(large); // O(n) --> Linear Time
 
 function logFirstTwoBoxes(boxes) {
-    console.log(boxes[0]);
-    console.log(boxes[1]);
+  console.log(boxes[0]);
+  console.log(boxes[1]);
 }
