75456ba0af11d4a494b25d5be384f351a6950cd8
bosshentai <baptistamhernani@gmail.com>
2021-02-07 21:06:11 -0100

    nemo have been finish it

--- nemo.js
+++ nemo.js
@@ -1,11 +1,30 @@
+const { performance } = require('perf_hooks');
+
 const nemo = ['nemo']
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt','darla','hank'];
+
+const large = new Array(100000).fill('nemo');
 
 function findNemo(array){
+   // let t0 = performance.now();
     for (let i = 0 ; i < array.length; i++){
         if (array[i] === 'nemo')
         console.log('Found Nemo!');
     }
+   // let t1 = performance.now()
 
+  //  console.log('Call to find Nemo took ' + (t1 - t0) + ' milliseconds')
 }
 
-findNemo(nemo);
+//findNemo(nemo);
+
+//findNemo(everyone);
+
+findNemo(large);  // O(n) --> Linear Time
+
+
+
+function logFirstTwoBoxes(boxes) {
+    console.log(boxes[0]);
+    console.log(boxes[1]);
+}
