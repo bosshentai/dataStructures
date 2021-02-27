ab1fad71a8b676117489ec73b22fbd5dc3133da5
bosshentai <baptistamhernani@gmail.com>
2021-02-27 12:18:49 -0100

    twitter add some changing

---
+++ Big_O/twitter.js
@@ -0,0 +1,23 @@
+// find 1st, Find Nth..
+const array = [
+  {
+    tweet: 'hi',
+    date: 2012,
+  },
+  {
+      tweet: 'my',
+      date: 2014,
+  },
+  {
+      tweet: 'teddy',
+      date: 2018
+  }
+];
+// O(n^2);
+
+'afafafdda'.length // depend of the linguage 
+// its a propriede in  js 
+// this a O(1)
+
+
+
